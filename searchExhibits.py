2d1dc8ff21a23c562560630ada55dd0781dcddac
Ajay Singh <singhajay08@gmail.com>
2018-11-25 14:04:21 -0500

    SelectExhibit

--- searchExhibits.py
+++ searchExhibits.py
@@ -1,3 +1,4 @@
+import tkinter
 from tkinter import *
 from PIL import ImageTk, Image
 import pymysql
@@ -7,7 +8,7 @@ from datetime import datetime, timedelta
 import decimal
 
 #=========Search Exhibit Window============
-class ATLzooSearchExhibits:
+class ATLzooSearchExhibits():
 
     def __init__(self):
         # Invoke createLoginWindow; Invoke buildLoginWindow, Set loginWindow as mainloop
@@ -25,46 +26,56 @@ class ATLzooSearchExhibits:
         self.searchExhibitWindow=Toplevel()
         self.searchExhibitWindow.title("Zoo Atlanta")
 
-    def buildSearchExhibitWindow(self,searchExhibitWindow):
+    def buildSearchExhibitWindow(self, searchExhibitWindow):
 
         # Title Label
-        titleLabel= Label(searchExhibitWindow,text = "Search Exhibits")
-        titleLabel.grid(row=1, column=2, sticky=W+E)
+        titleLabel= Label(searchExhibitWindow,text = "Search Exhibits", font = "Verdana 16 bold ")
+        titleLabel.grid(row=0, column=1)
 
         # Labels
         numAnimalsLabel= Label(searchExhibitWindow,text = "Number of Animals")
-        numAnimalsLabel.grid(row=2,column=1)
+        numAnimalsLabel.grid(row=2,column=2)
+        nameLabel= Label(searchExhibitWindow,text = "Name")
+        nameLabel.grid(row=2,column=0)
+
+        # Entry
+        self.exhibitNameSV = StringVar()
+        exhibitNameEntry = Entry(searchExhibitWindow, textvariable=self.exhibitNameSV, width=20)
+        exhibitNameEntry.grid(row=2, column=1)
+
         waterFeatureLabel= Label(searchExhibitWindow,text = "Water Feature")
-        waterFeatureLabel.grid(row=3,column=1)
+        waterFeatureLabel.grid(row=3,column=2)
         sizeLabel= Label(searchExhibitWindow,text = "Size")
-        sizeDateLabel.grid(row=4,column=1)
+        sizeLabel.grid(row=3,column=0)
 
         # Button
         findExhibitsButton = Button(searchExhibitWindow, text="Find Exhibits", command=self.searchExhibitWindowFindExhibitsButtonClicked)
-        findExhibitsButton.grid(row=5,column=3)
+        findExhibitsButton.grid(row=6,column=3)
 
         backButton = Button(searchExhibitWindow, text="Back", command=self.searchExhibitWindowBackButtonClicked)
-        backButton.grid(row=5,column=1)
+        backButton.grid(row=6,column=1)
 
-
+        selectExhibitTree = ttk.Treeview(searchExhibitWindow, columns=("Name", "Size", "NumAnimals", "Water"))
+        # self.selectExhibitTree['show'] = "headings"
+        selectExhibitTree.heading('#0', text = "Name")
+        selectExhibitTree.heading('#1', text = "Size")
+        selectExhibitTree.heading('#2', text = "NumAnimals")
+        selectExhibitTree.heading('#3', text = "Water")
+        selectExhibitTree.column('#0', width = 150, anchor = "center")
+        selectExhibitTree.column('#1', width = 150, anchor = "center")
+        selectExhibitTree.column('#2', width = 150, anchor = "center")
+        selectExhibitTree.column('#3', width = 150, anchor = "center")
+        selectExhibitTree.grid(row=5, columnspan=4, sticky = 'nsew')
+        
 
     def searchExhibitWindowFindExhibitsButtonClicked(self):
 
-        self.departsFrom = self.departsFromSV.get()
-        self.arrivesAt = self.arrivesAtSV.get()
-        self.departureDate = self.departureDateSV.get()
+        self.min = self.minSV.get()
+        self.max = self.maxSV.get()
+        self.name = self.nameSV.get()
 
-        if self.departsFrom == self.arrivesAt:
-            messagebox.showwarning("Error", "Your departure station and destination are the same.")
-            return False
-        if datetime.strptime(self.departureDate, '%Y-%m-%d').date() < datetime.today().date():
-            messagebox.showwarning("Error", "Departure date cannot be earlier than today")
-            return False
-
-        try:
-            datetime.strptime(self.departureDate, '%Y-%m-%d')
-        except ValueError:
-            messagebox.showwarning("Error", "Departure date is not valid. (yyyy-mm-dd)")
+        if self.min == self.max:
+            # messagebox.showwarning("Error", "Your departure station and destination are the same.")
             return False
 
         self.searchExhibitWindow.destroy()
@@ -76,4 +87,5 @@ class ATLzooSearchExhibits:
         self.chooseFunctionalityWindow.deiconify()
 
 
-a=ATLzooSearchExhibits()
+
+d=ATLzooSearchExhibits()
